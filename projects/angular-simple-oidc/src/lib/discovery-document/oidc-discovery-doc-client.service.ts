03596d3a1aede7b192f005b3dbef0013bf93cabe
Leonardo Chaia <lchaia@astonishinglab.com>
2019-08-08 14:35:14 -0300

    refactor: use shareReplay for memory caches

--- projects/angular-simple-oidc/src/lib/discovery-document/oidc-discovery-doc-client.service.ts
+++ projects/angular-simple-oidc/src/lib/discovery-document/oidc-discovery-doc-client.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, catchError, shareReplay } from 'rxjs/operators';
 import { DiscoveryDocument, JWTKeys } from '../core/models';
 import { urlJoin } from '../utils/url-join';
 import { AuthConfigService } from '../config/auth-config.service';
@@ -9,51 +8,31 @@ import { AuthConfigService } from '../config/auth-config.service';
 @Injectable()
 export class OidcDiscoveryDocClient {
 
-    public readonly discoveryDocumentAbsoluteEndpoint = urlJoin(
-        this.config.configuration.openIDProviderUrl,
+    public get discoveryDocumentAbsoluteEndpoint() {
+        return urlJoin(this.config.configuration.openIDProviderUrl,
         this.config.configuration.discoveryDocumentUrl);
-
-    public get current$() {
-        if (this.subject.value != null) {
-            return this.subject.asObservable();
-        } else {
-            console.info('Obtaining discovery document..');
-            return this.requestDiscoveryDocument()
-                .pipe(switchMap(doc => {
-                    this.subject.next(doc);
-                    return this.subject.asObservable();
-                }));
         }
-    }
 
-    public get jwtKeys$() {
-        if (this.jwtKeysSubject.value != null) {
-            return this.jwtKeysSubject.asObservable();
-        } else {
-            return this.current$.pipe(
-                switchMap(doc => {
-                    console.info('Obtaining JWT Keys..');
-                    return this.requestJWTKeys(doc)
-                        .pipe(switchMap(keys => {
-                            this.jwtKeysSubject.next(keys);
-                            return this.jwtKeysSubject.asObservable();
-                        }));
-                }));
-        }
-    }
+    public readonly current$ = this.requestDiscoveryDocument()
+        .pipe(shareReplay({ refCount: true, bufferSize: 1 }));
 
-    protected subject = new BehaviorSubject<DiscoveryDocument>(null);
-    protected jwtKeysSubject = new BehaviorSubject<JWTKeys>(null);
+    public readonly jwtKeys$ = this.current$
+        .pipe(
+            switchMap(doc => this.requestJWTKeys(doc)),
+            shareReplay({ refCount: true, bufferSize: 1 })
+        );
 
     constructor(
         protected readonly config: AuthConfigService,
         protected readonly http: HttpClient) { }
 
     public requestDiscoveryDocument() {
-        return this.http.get<DiscoveryDocument>(this.discoveryDocumentAbsoluteEndpoint);
+        console.info('Obtaining discovery document');
+        return this.http.get<DiscoveryDocument>(this.discoveryDocumentAbsoluteEndpoint)
     }
 
     public requestJWTKeys(doc: DiscoveryDocument) {
-        return this.http.get<JWTKeys>(doc.jwks_uri);
+        console.info('Obtaining JWT Keys');
+        return this.http.get<JWTKeys>(doc.jwks_uri)
     }
 }
