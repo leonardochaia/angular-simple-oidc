689ab5748a0ffd420b6a62e6465ad6f03d188fc7
Leonardo Chaia <lchaia@outlook.com>
2021-10-06 12:59:24 -0700

    fix(operators): filterInstanceOf loosing source type

--- projects/angular-simple-oidc/operators/lib/filter-instance-of.ts
+++ projects/angular-simple-oidc/operators/lib/filter-instance-of.ts
@@ -1,7 +1,6 @@
 import { Type } from '@angular/core';
 import { filter } from 'rxjs/operators';
-import { MonoTypeOperatorFunction } from 'rxjs';
 
-export function filterInstanceOf<T>(type: Type<T>): MonoTypeOperatorFunction<T> {
-    return filter<T>((e): e is T => e instanceof type);
+export function filterInstanceOf<T>(type: Type<T>) {
+    return filter((e): e is T => e instanceof type);
 }
